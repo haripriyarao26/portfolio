f13ad8f982316fad0ac072afccfcb668d803880c
haripriyarao26 <haripriyarao26@users.noreply.github.com>
2025-11-22 18:59:15 -0800

    Configure GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
 }
 
 module.exports = nextConfig
